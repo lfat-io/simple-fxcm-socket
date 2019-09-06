d100b4d227286f995a5e870802125bb45d1c3d2e
sefatunckanat <sefatunckanat73@gmail.com>
2019-09-06 10:35:16 +0300

    edit

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 tar czf simple_fxcm.tar.gz app package.json package-lock.json
-scp simple_fxcm.tar.gz root@206.189.240.54:/home
+scp simple_fxcm.tar.gz root@206.189.240.00:/home
 rm simple_fxcm.tar.gz
 
-ssh root@206.189.240.54 << 'ENDSSH'
+ssh root@206.189.240.00 << 'ENDSSH'
 cd /home
 rm -rf simple_fxcm
 pm2 stop simple_fxcm
